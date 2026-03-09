cf42420369a6c20505d054a55642357a6150ff95
pantavit-h <pantavit.h@craft-itech.co>
2026-03-09 13:16:29 +0700

    add Cart page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Select from "./pages/Select";
 import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/select" element={<Select />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Cart.tsx
@@ -0,0 +1,71 @@
+import TabBar from "../components/TabBar";
+import styles from "./css/Cart.module.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Cart() {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.cartpage}>
+      <div className={styles.header}>
+        <button className={styles.backBtn} onClick={() => navigate(-1)}>
+          ←
+        </button>
+
+        <div className={styles.title}>ตะกร้า</div>
+      </div>
+
+      <div className={styles.cartItem}>
+        <img
+          className={styles.productImg}
+          src="https://images.unsplash.com/photo-1503431128871-cd250803fa41?w=400"
+        />
+
+        <div className={styles.productInfo}>
+          <div className={styles.productName}>อาหารเปียกพรีเมียม 6 ซอง</div>
+
+          <div className={styles.productDesc}>เนื้อแน่น ไม่เค็ม</div>
+
+          <div className={styles.bottomRow}>
+            <div className={styles.productPrice}>279 THB</div>
+
+            <div className={styles.qtyBox}>
+              <button className={styles.qtyBtn}>-</button>
+              <div className={styles.qtyNumber}>4</div>
+              <button className={styles.qtyBtn}>+</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.paymentBox}>
+        <div className={styles.paymentTitle}>ข้อมูลการชำระเงิน</div>
+
+        <div className={styles.paymentRow}>
+          <span className={styles.paymentLabel}>รวมการสั่งซื้อ</span>
+          <span className={styles.paymentValue}>837 THB</span>
+        </div>
+
+        <div className={styles.paymentRow}>
+          <span className={styles.paymentLabel}>การจัดส่ง</span>
+          <span className={styles.paymentValue}>25 THB</span>
+        </div>
+
+        <div className={styles.divider}></div>
+
+        <div className={styles.paymentTotal}>
+          <span>ยอดชำระเงินทั้งหมด</span>
+          <span>862 THB</span>
+        </div>
+      </div>
+
+      <div className={styles.checkoutBar}>
+        <div className={styles.checkoutPrice}>304 THB</div>
+
+        <button className={styles.checkoutBtn}>ชำระเงิน</button>
+      </div>
+
+      <TabBar />
+    </div>
+  );
+}
